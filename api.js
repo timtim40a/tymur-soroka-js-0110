806f1ee1dca4f04197f047edb58c0a77c098c4ef
timtim40a <timtim40a@gmail.com>
2025-10-01 15:20:52 +0100

    api connected - image of the day

---
+++ api.js
@@ -0,0 +1,53 @@
+const apiKey = '5ad9f1c9dd254f1baa5b3b8d5a3ebbd9'
+
+async function getArtwork(query = '') {
+    if (query == '') {
+        try {
+            const response = await fetch(
+                'https://api.artsearch.io/artworks/random/?api-key=5ad9f1c9dd254f1baa5b3b8d5a3ebbd9'
+            )
+            const data = await response.json()
+            console.log(data)
+
+            if (data) {
+                return data.image
+
+                /*{
+                    id: artwork.id,
+                    title: artwork.title,
+                    image: artwork.image,
+                    description: artwork.description,
+                }*/
+            }
+
+            console.log('The image of the day has been successfully loaded')
+        } catch {
+            ;('Error fetching the artwork of the day.')
+        }
+    } else {
+        try {
+            const response = await fetch(
+                `https://api.artsearch.io/artworks/?query=${query}&number=1&api-key=5ad9f1c9dd254f1baa5b3b8d5a3ebbd9`
+            )
+            const data = await response.json()
+            console.log(data)
+
+            if (data) {
+                return data.image
+
+                /*{
+                    id: artwork.id,
+                    title: artwork.title,
+                    image: artwork.image,
+                    description: artwork.description,
+                }*/
+            }
+
+            console.log('The image by query has been successfully loaded')
+        } catch {
+            ;('Error fetching the artwork by query.')
+        }
+    }
+}
+
+export default getRandomArtwork
--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 import Tabs from './tabs.js'
+import getRandomArtwork from './api.js'
 
 const tabs = new Tabs('.tab')
+const featuredTab = document.querySelector('.tab__content[data-tab="featured"]')
+const searchTab = document.querySelector('.tab__content[data-tab="search"]')
+
+async function getImageOfTheDay() {
+    const imgOfTheDay = document.createElement('img')
+    const artwork = await getRandomArtwork()
+    imgOfTheDay.src = artwork
+    imgOfTheDay.classList.add('artwork')
+    featuredTab.appendChild(imgOfTheDay)
+}
+
+async function getImageByQuery() {
+    const imgByQuery = document.createElement('img')
+    const artwork = await getRandomArtwork()
+    imgByQuery.src = artwork
+    imgByQuery.classList.add('artwork')
+    featuredTab.appendChild(imgByQuery)
+}
+
+getImageOfTheDay()
