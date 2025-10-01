1c79b130a179a605e78352c2f8eb59f5d8050c00
timtim40a <timtim40a@gmail.com>
2025-10-01 16:17:53 +0100

    added daily pictures to the localStorage

--- main.js
+++ main.js
@@ -6,14 +6,32 @@ const featuredTab = document.querySelector('.tab__content[data-tab="featured"]')
 const searchTab = document.querySelector('.tab__content[data-tab="search"]')
 const formSubmitButton = document.getElementById('search-button')
 
+var today = new Date()
+var dd = String(today.getDate()).padStart(2, '0')
+var mm = String(today.getMonth() + 1).padStart(2, '0') //January is 0!
+var yyyy = today.getFullYear()
+
+today = mm + '/' + dd + '/' + yyyy
+
+console.log(today)
+
 formSubmitButton.addEventListener('click', (event) => getImageByQuery(event))
 
 async function getImageOfTheDay() {
     const imgOfTheDay = document.createElement('img')
-    const artwork = await getRandomArtwork()
-    imgOfTheDay.src = artwork
-    imgOfTheDay.classList.add('artwork')
-    featuredTab.appendChild(imgOfTheDay)
+    const srcOfTheDay = localStorage.getItem(today)
+    if (srcOfTheDay) {
+        const artwork = srcOfTheDay
+        imgOfTheDay.src = artwork
+        imgOfTheDay.classList.add('artwork')
+        featuredTab.appendChild(imgOfTheDay)
+    } else {
+        const artwork = await getRandomArtwork()
+        imgOfTheDay.src = artwork
+        imgOfTheDay.classList.add('artwork')
+        featuredTab.appendChild(imgOfTheDay)
+        localStorage.setItem(today, artwork)
+    }
 }
 
 async function getImageByQuery(event) {
@@ -21,13 +39,16 @@ async function getImageByQuery(event) {
     const query = document.getElementById('search-input').value
     console.log(query)
     const artwork = await getRandomArtwork(query)
+    // if image element has not been created yet: create it and set class, id and source
     if (!document.getElementById('image-by-query')) {
         const imgByQuery = document.createElement('img')
         imgByQuery.classList.add('artwork')
         imgByQuery.setAttribute('id', 'image-by-query')
         searchTab.appendChild(imgByQuery)
         imgByQuery.src = artwork
-    } else {
+    }
+    // otherwise just set the source
+    else {
         const imgByQuery = document.getElementById('image-by-query')
         imgByQuery.src = artwork
     }
